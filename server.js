b998899b3d2436298bf8e063c4c4f22fe6a21f97
vky3831 <vikashyadav2210@gmail.com>
2021-03-28 02:25:20 +0530

    config PORT

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ io.on('connection', socket => {
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
